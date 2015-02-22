e642c3d5d3c4a5a86224b3527b4f775fb4e6ec5f
kadlugan <nick@fido.local>
2015-02-23 10:17:54 +1100

    fixed syntax error

--- run_project.py
+++ run_project.py
@@ -89,10 +89,9 @@ class RunProjectCommand(sublime_plugin.WindowCommand):
       # for the system error messages
       self.window.run_command('exec', {'cmd': cmd, "file_regex": "^[^/]*(/[^:]*):([0-9]+):([0-9]*)(.*)$" })
       # for my generated error messages
-      self.window.run_command('exec', {'cmd': cmd, 
-                                      "file_regex": "°(.*):.*:([0-9]+):",
-                                      "working_dir": "${project_path}"
-                                      # "working_dir": "${project_path:${folder}}"
-                                      }
+      self.window.run_command('exec', { 'cmd': cmd,  
+                                        "file_regex": "°(.*):.*:([0-9]+):", 
+                                        "working_dir": "${project_path}" 
+                                      })                                      }
     else: # windows
       self.window.run_command('exec', {'cmd': cmd, "file_regex": "(?i)^[^C-Z]*([C-Z]:[^:]*):([0-9]+):([0-9]*)(.*)$" })
